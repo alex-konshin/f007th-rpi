1669d88cdb403a156c7249db507817d6f0112feb
Alex Konshin (home) <akonshin@ptc.com>
2019-01-05 01:04:04 -0500

    Fixed build script for RPi

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ BUILD_HOME=`cd $BUILD_HOME; pwd`
 cd ${BUILD_HOME}/f007th-rpi_send
 make f007th-rpi_send
 status=$?
+if [ ! -d ${BUILD_HOME}/bin ]; then mkdir ${BUILD_HOME}/bin; fi
 if [ "$status" = 0 ]; then cp f007th-rpi_send ${BUILD_HOME}/bin/; fi
 
 cd ${BUILD_HOME}/f007th-ts-httpd-rpi
