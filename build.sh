a2a1f73f850351d8941e7a80e4ebfe87f88c837b
Alex Konshin <akonshin@gmail.com>
2021-02-21 15:29:14 -0500

    Renamed build.cfg to build.cfg.sample to prevent conflict when pulling changes

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ case $machine in
   *)
     echo "ERROR: Unrecognized platform (\`uname -m\` = ${machine})."
     exit 1
-    ;;    
+    ;;
 esac
 if [ $is_ARM -eq 1 -a -e '/proc/device-tree/model' ]; then
   model=$(tr -d '\0' </proc/device-tree/model)
@@ -49,7 +49,7 @@ if [ $is_ARM -eq 1 -a -e '/proc/device-tree/model' ]; then
     if [[ $model = Raspberry* ]]; then
       is_RPI=1
       echo "Building on ${model}"
-    fi 
+    fi
   fi
 fi
 if [ $is_RPI -ne 1 ]; then
@@ -69,7 +69,7 @@ fi
 # -----------------------------------------------------------------------------
 
 BUILD_DIR=${BUILD_HOME}/build
-if [ -e ${BUILD_DIR} ]; then 
+if [ -e ${BUILD_DIR} ]; then
   rm -r ${BUILD_DIR}
 fi
 cp -R ${BUILD_HOME}/f007th-ts-all ${BUILD_DIR}
@@ -107,14 +107,14 @@ fi
 if [ $is_RPI -eq 1 ]; then
   options='-DRPI '${options}
 elif [ $is_turbot -eq 1 ]; then
-  platform_options=' -U_FORTIFY_SOURCE -fno-stack-protector'    
+  platform_options=' -U_FORTIFY_SOURCE -fno-stack-protector'
 fi
 
 # -----------------------------------------------------------------------------
 process_subdir_mk() {
   file="${BUILD_DIR}/$1"
-  echo "... Procesing file \"${file}\" ..." 
-  
+  echo "... Procesing file \"${file}\" ..."
+
 #  g++ -std=c++11 -DRPi               -DNDEBUG -DINCLUDE_HTTPD -DINCLUDE_POLLSTER                -O2     -Wall -c -fmessage-length=0 -pthread                                        -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 #  g++ -std=c++11 -DRPI -DUSE_GPIO_TS -DNDEBUG -DINCLUDE_HTTPD -DINCLUDE_POLLSTER -DINCLUDE_MQTT -O2     -Wall -c -fmessage-length=0 -pthread                                        -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 #  g++ -std=c++11       -DUSE_GPIO_TS -DNDEBUG -DINCLUDE_HTTPD -DINCLUDE_POLLSTER                -O2     -Wall -c -fmessage-length=0 -pthread -U_FORTIFY_SOURCE -fno-stack-protector -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
@@ -133,7 +133,7 @@ process_subdir_mk() {
       line_filter='!/^\#.*/ && !/\.?\.\/protocols\/DS18B20\..*/'
     fi
   fi
-  
+
   awk_program=${line_filter}' { if (/g\+\+/) { print "'${compile}'"} else { print } }'
 
   #echo "${awk_program}"
@@ -161,8 +161,8 @@ process_subdir_mk "subdir.mk"
 # -----------------------------------------------------------------------------
 process_objects_mk() {
   file="${BUILD_DIR}/$1"
-  echo "... Procesing file \"${file}\" ..." 
-  
+  echo "... Procesing file \"${file}\" ..."
+
   #  LIBS := -lrt -lcurl -lmicrohttpd -lmosquitto -lmosquittopp
   libs='LIBS := -lrt -lcurl'
   if [ $use_gpio_ts -ne 1 ]; then
@@ -174,7 +174,7 @@ process_objects_mk() {
   if [ $include_MQTT -eq 1 ]; then
     libs=${libs}' -lmosquitto -lmosquittopp'
   fi
-  
+
   awk_program='!/^\#.*/ { if (/^LIBS \:=/) { print "'${libs}'"} else { print } }'
   #echo "${awk_program}"
   awk "${awk_program}" "${file}" > "${file}.new"
@@ -201,13 +201,13 @@ process_makefile() {
     return 0
   fi
   file="${BUILD_DIR}/$1"
-  echo "... Procesing build script \"${file}\" ..." 
-  
+  echo "... Procesing build script \"${file}\" ..."
+
   # all: f007th-send
   # f007th-send: $(OBJS) $(USER_OBJS)
   #   g++ -pthread -o "f007th-send" $(OBJS) $(USER_OBJS) $(LIBS)
   #   -$(RM) $(CC_DEPS)$(C++_DEPS)$(EXECUTABLES)$(OBJS)$(C_UPPER_DEPS)$(CXX_DEPS)$(C_DEPS)$(CPP_DEPS) f007th-send
-  
+
   awk_program='!/^\#.*/ { if (/f007th\-send/) { sub(/f007th\-send/,"'${executable}'") }; print }'
   #echo "${awk_program}"
   awk "${awk_program}" "${file}" > "${file}.new"
